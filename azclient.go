d8ab0dd2a05205b47f664f44f1635232f2de0787
Nicola Gallo <nicolagmt@hotmail.com>
2025-02-24 14:51:12 +0100

    refactor: refactor: working on grpc code refactoring

--- azclient.go
+++ azclient.go
@@ -17,11 +17,12 @@
 package permguard
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/permguard/permguard-go/az/azreq"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"github.com/permguard/permguard-go/az/azreq"
 
 	"github.com/permguard/permguard-go/internal/az/azreq/grpc/v1"
 )
@@ -49,17 +50,32 @@ func NewAZClient(opts ...AZOption) *AZClient {
 // Check checks the input authorization request with the authorization server.
 func (c *AZClient) Check(req *azreq.AZRequest) bool {
 	target := fmt.Sprintf("%s:%d", c.azConfig.pdpEndpoint.endpoint, c.azConfig.pdpEndpoint.port)
-	conn, err := grpc.Dial(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		return false
 	}
 	client := v1.NewV1PDPServiceClient(conn)
-	azCheckRequest := &v1.AuthorizationCheckRequest{
-		AuthorizationModel: &v1.AuthorizationModelRequest{
-			ZoneID: int64(req.GetAuthZModel().GetZoneID()),
-		},
-	}
-	azCheckResponse, err := client.AuthorizationCheck(nil, azCheckRequest)
+	azCheckRequest := &v1.AuthorizationCheckRequest{}
+	
+	azCheckRequest.AuthorizationModel = &v1.AuthorizationModelRequest{}
+	azCheckRequest.AuthorizationModel.ZoneID = int64(req.GetAuthZModel().GetZoneID())
+	azCheckRequest.AuthorizationModel.PolicyStore = &v1.PolicyStore{}
+	azCheckRequest.AuthorizationModel.PolicyStore.ID = "abc"
+	azCheckRequest.AuthorizationModel.PolicyStore.Kind = "ledger"
+
+	azCheckRequest.Subject = &v1.Subject{}
+	azCheckRequest.Subject.ID = "pippo"
+	azCheckRequest.Subject.Kind = "user"
+
+	azCheckRequest.Resource = &v1.Resource{}
+	azCheckRequest.Resource.ID = "pippo"
+	azCheckRequest.Resource.Kind = "user"
+
+	azCheckRequest.Action = &v1.Action{}
+	azCheckRequest.Action.Name = "pippo"
+
+	ctx := context.Background()
+	azCheckResponse, err := client.AuthorizationCheck(ctx, azCheckRequest)
 	if err != nil {
 		return false
 	}
